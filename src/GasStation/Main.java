662ea4e3c1f52d8ce04898c82c45d5e4422d0e34
Pranav Kural <pranavkural042@gmail.com>
2017-04-21 21:29:35 -0400

    Testing the working of PumpDB and PumpManagement by printing out gas prices

--- src/GasStation/Main.java
+++ src/GasStation/Main.java
@@ -3,5 +3,19 @@ package GasStation;
 // import PumpSoftware classes required to run the GasStation
 import PumpSoftware.*;
 
+/**
+ * Main Class to run the Gas Station and utilize all of the application logic
+ */
 public class Main {
+
+  // Main method
+    public static void main(String[] args) {
+
+        // Instantiate a new instance of the PumpManagement Class to test the printing out of gas prices
+        PumpManagement pumpManagementHub = new PumpManagement();
+
+        // print out the gas prices information
+        System.out.println(pumpManagementHub);
+    }
+
 }
