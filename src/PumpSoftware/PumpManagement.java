204fc0615a9b6cfb0e65c0ad5633ffa529af368e
Pranav Kural <pranavkural042@gmail.com>
2017-04-21 21:29:05 -0400

    retrieving the gas prices from database using the db service from PumpDB

--- src/PumpSoftware/PumpManagement.java
+++ src/PumpSoftware/PumpManagement.java
@@ -1,4 +1,50 @@
 package PumpSoftware;
 
+// import the services to work with Pump's database
+import PumpDB.db;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Pump Management class to manage the prices of the gases
+ */
 public class PumpManagement {
+
+    // gas price variables
+    private double regularPrice;
+    private double plusPrice;
+    private double supremePrice;
+
+    // constructor
+    public PumpManagement() {
+        // get the prices of the gases
+        getGasPrices();
+    }
+
+    // get gas prices from the database
+    private void getGasPrices() {
+
+        // get gas prices
+        ArrayList<Double> gasPrices = db.getGasPrices();
+
+        // set the gas prices
+        regularPrice = gasPrices.get(2);    // price for regular gas type
+        plusPrice = gasPrices.get(1);       // price for plus gas type
+        supremePrice = gasPrices.get(2);    // price for plus
+
+    }
+
+    // Override toString method to print out gas information
+    @Override
+    public String toString() {
+        // for decimal formatting, getting 2 decimal places
+        DecimalFormat df = new DecimalFormat("#.00");
+        return String.format("------%s------%n%s: %s%n%s: %s%n%s: %s%n",
+                "Gas Prices",
+                "Regular", df.format(regularPrice),
+                "Plus", df.format(plusPrice),
+                "Supreme", df.format(supremePrice));
+    }
 }
