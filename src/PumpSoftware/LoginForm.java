98d3be92f6d301a8ede9494d05e00be95fd36512
Pranav Kural <pranavkural042@gmail.com>
2017-04-22 15:55:07 -0400

    Built the initial panels for login form

--- src/PumpSoftware/LoginForm.java
+++ src/PumpSoftware/LoginForm.java
@@ -1,4 +1,44 @@
 package PumpSoftware;
 
-public class LoginForm {
+// import the services to work with Pump's database
+import PumpDB.db;
+import Utilities.Services;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginForm extends JFrame {
+
+    private String[] userData = new String[3];
+
+    private Services services = new Services();
+
+    public LoginForm() {
+
+        // Generate the login form
+        generateLoginForm();
+    }
+
+    private void generateLoginForm() {
+
+        // Root frame layout
+        setLayout(new BorderLayout());
+
+        // Top panel
+        JPanel topPanel = new JPanel();
+        JLabel lblLogo = new JLabel(services.createImageIcon("Resources/Esso.png", "Logo image of the pump"), JLabel.CENTER);
+        topPanel.add(lblLogo);
+        add(topPanel, BorderLayout.NORTH);
+
+        // Center panel
+        JPanel centerPanel = new JPanel(new GridLayout(2, 2));
+        JLabel lblUserId = new JLabel("User ID: ", JLabel.LEFT);
+
+    }
+
+    private void getUserData() {
+        // get the user data
+        userData = db.getUserDetails("10000");
+    }
+
 }
