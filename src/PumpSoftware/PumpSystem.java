54b048c2066470cff5bd179ee52a2031eb62bd0e
Pranav Kural <pranavkural042@gmail.com>
2017-04-24 19:28:03 -0400

    Added the handler to update the globalTank form when the tab pressed

--- src/PumpSoftware/PumpSystem.java
+++ src/PumpSoftware/PumpSystem.java
@@ -1,6 +1,8 @@
 package PumpSoftware;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class PumpSystem extends JFrame {
 
@@ -29,10 +31,26 @@ public class PumpSystem extends JFrame {
         // initialize the tab container
         tabContainer = new JTabbedPane();
 
+        // reference to the global tank
+        GlobalTank globalTank = new GlobalTank();
+
         // add the tabs
         tabContainer.addTab("PumpSystem Management", null, (loginForm.isManager() ? new PumpManagement() : new JLabel("You don't have access to this tab. You need managerial privileges to update the gas prices.", JLabel.CENTER)), "Update the gas prices.");
-        tabContainer.addTab("Global Tank", null, new GlobalTank(), "Check the gas prices and quantity.");
+        tabContainer.addTab("Global Tank", null, globalTank, "Check the gas prices and quantity.");
         tabContainer.addTab("Launch PumpSystem", null, new JLabel("The pump will be launched soon.", JLabel.CENTER), "Fill gas.");
+
+        tabContainer.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (tabContainer.getSelectedIndex() == 1) {
+                    globalTank.displayGasQuantityInfo();
+                    globalTank.displayGasPriceInfo();
+                } else if(tabContainer.getSelectedIndex() == 2) {
+                    Pump pump = new Pump();
+                }
+            }
+        });
+
 
         // add the tabContainer to PumpSystem GUI
         add(tabContainer);
